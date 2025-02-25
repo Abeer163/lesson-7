654a56709dca79f195047e6ede82200b02bd7280
Abeer163 <rehmanmughis@gmail.com>
2025-02-25 17:42:01 +0000

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,10 @@
+#right shift
+a = 5
+b = -7
+print("a >> 1 = ", a >> 1)
+print("b >> 1 = ", b >> 1)
+#left shift
+a = 7
+b = -4
+print("a << 1 = ", a << 1)
+print("b << 1 = ", b << 1)
---
+++ activity3.py
@@ -0,0 +1,13 @@
+#membership operators in and not in
+a = "hello world"
+b = "world"
+if b in a:
+    print("true value is present")
+else:
+    print("false value is not present")
+x = "hi Abeer"
+y = "hi"
+if y not in x:
+    print("true")
+else:
+    print("false")
